5843fa530767023ae4a777b105949ee49ff2aa99
sajidahw <sajidahwahdy@Sajidahs-MacBook-Air.local>
2024-02-21 00:42:23 -0800

    Added main file, README, etc

---
+++ README.md
@@ -0,0 +1,74 @@
+# Weather Microservice
+
+This is a simple weather microservice that provides weather information for a given location using the geographical coordinates of latitude and longitude. After receiving the coordinates passed from my partner's app, I query the OpenWeather API to get the weather information as a middleman. The weather information is then returned to the partner's app in JSON format.
+
+</br>
+
+## How to clone and run weather microservice files
+
+```bash
+git clone GITHUB REPO URL
+cd weather_microservice
+npm install
+```
+
+</br>
+
+To run the weather microservice, create an OpenWeather API key (after creating an account) to save within an .env file at the root file. Then run using the following command in a new terminal to start the Express server:
+
+```bash
+node server.js
+```
+
+</br>
+
+## How to <i>request</i> data from the weather microservice
+
+The partner's app will send a GET request to the weather microservice with the geographical coordinates of the location.
+
+Example http request call with endpoint would be:
+
+```javascript
+`GET/weather_microservice`;
+```
+
+The accompanying latitude and longitude passed through the request will save the coordinates as
+
+```javascript
+ lat=${lat}&lon=${lon}
+```
+
+The weather microservice will then send a GET API request to the OpenWeather API to get the weather information for the location passing along the geo coordinates with an API key.
+
+In another terminal, using a command line to make network requests, type curl + endpoint url to http request weather data according to the passed coordinates.
+
+Example http request in order to receive data to generate a response for San Francisco's coordinates using "curl" + endpoint url:
+
+```javascript
+curl "localhost:8080/weather_microservice?lat=37.7749&lon=122.4194"
+```
+
+</br>
+
+## How to <i>receive</i> data from the weather microservice
+
+The requested data will be sent back as a JSON object as a response from the GET HTTP method as it was requested earlier.
+
+<br>
+
+The weather information will be returned to the partner's app as a JSON object displaying the format of the desired fields for weather's short description, temperature in Fahrenheit and a weather icon.
+
+<br>
+From the example above using San Francisco's geo coordinates, the following JSON object will be returned:
+
+```json
+{
+  "description": "Clouds",
+  "temperature": 31,
+  "icon": "04d"
+}
+```
+
+</br>
+
+## UML sequence diagram for the weather microservice
---
+++ server.js
@@ -0,0 +1,69 @@
+// this server is for my microservice I've created for my partner's project.
+// he will be able to pass me his city's coordinates thru a http get request with the {lat}&{lon} saved as variables.
+// I will then use those coordinates to fetch the weather data from the OpenWeather API and return it to him in a JSON format; effectively acting like the middleman between him and the OpenWeather API.
+
+// Setting up the Server //
+require("dotenv").config();
+const express = require("express");
+const axios = require("axios");
+const app = express(); // create an instance of express
+const PORT = process.env.PORT || 8080; // set the port to 8080
+const OPENWEATHER_API_KEY = process.env.OPENWEATHER_API_KEY;
+const OPENWEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// URL Endpoint to get weather by city coordinates
+// fetch the weather data by coordinates from Alex's microservice
+app.get(`/weather_microservice`, async (req, res) => {
+  // saved the passed along coordinates of latitude and longitude from the query parameters
+  const lat = req.query.lat;
+  const lon = req.query.lon;
+
+  // Checking if the lat and lon parameters are present
+  if (!lat || !lon) {
+    return res
+      .status(400)
+      .send("Error: The latitude and longitude parameters are missing!");
+  }
+  // console.log(OPENWEATHER_API_URL);
+  try {
+    // Fetching weather data from OpenWeather API as middleman & saving retrieved weather data to var to retain it
+    const coordWeatherData = await axios.get(
+      `${OPENWEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${OPENWEATHER_API_KEY}&units=imperial`
+    );
+
+    // console.log(
+    //   `${OPENWEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${OPENWEATHER_API_KEY}&units=imperial`
+    // );
+
+    // retrieving specific data elements which Alex desires instead of all data
+    const shortCoordWeatherData = {
+      city: coordWeatherData.data.sys.name,
+      description: coordWeatherData.data.weather[0].main,
+      temperature: Math.round(coordWeatherData.data.main.temp),
+      icon: coordWeatherData.data.weather[0].icon,
+      // icon: src={`https://openweathermap.org/img/wn/${data.weather[0].icon}@4x.png`}
+    };
+
+    res.status(200).json(shortCoordWeatherData); // sending weather back to Alex as JSON object
+
+    // console.log(coordWeatherData.data.main.temp);
+  } catch (error) {
+    console.error(error);
+    res
+      .status(500) // API call error
+      .send(
+        "API Error: Something went wrong when retrieving the weather data!"
+      );
+  }
+});
+
+// Listen on the port //
+app.listen(PORT, () => {
+  console.log(
+    ` 🌤️ Weather Microservice Server is running on http://localhost:${PORT}; 
+    press Ctrl-C to terminate.`
+  );
+});
